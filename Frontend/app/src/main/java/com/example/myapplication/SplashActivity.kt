c49bb23cdf3edfcd20076064e1cc483a296f38d6
Ali Bawa <alibawa@Alis-Air.fritz.box>
2025-05-08 01:04:55 +0200

    Initial page commit

---
+++ Frontend/app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View.OnCreateContextMenuListener
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+
+}

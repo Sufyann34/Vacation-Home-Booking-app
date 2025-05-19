016bc9f501ab96e581dfa1a1d7bcc086c70d50df
Mohammad Hasan <syedmohammad.15764@gmail.com>
2025-05-20 00:11:13 +0200

    Raw JSON format could be seen in the application

--- hotel_application/app/src/main/java/com/example/hotel_application/MainActivity.kt
+++ hotel_application/app/src/main/java/com/example/hotel_application/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.hotel_application.ui.theme.Hotel_applicationTheme
 import com.example.hotel_application.viewModel.MainViewModel
@@ -23,38 +23,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Hotel_applicationTheme {
-                Surface (
+                Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val mainViewModel = viewModel<MainViewModel>()
                     val state = mainViewModel.state
 
-                    Text(text = "Hotels: ${state.hotels.size}")
+                    // Make the content scrollable
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .verticalScroll(rememberScrollState()) // Enables scrolling
+                            .padding(16.dp) // Adds padding to the content
+                    ) {
+                        // Display raw data as string
+                        Text(text = state.hotels.toString())
+                    }
                 }
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
             }
         }
     }
 }
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    Hotel_applicationTheme {
-//        Greeting("Android")
-//    }
-//}
